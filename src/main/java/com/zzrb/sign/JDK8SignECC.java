6a9da8ec22b76a65bcc89904895ee1ffeda4076b
wangkuanzzu <wangkuan.zzu@qq.com>
2020-03-02 14:57:08 +0800

    api-sdk project init

---
+++ src/main/java/com/zzrb/sign/JDK8SignECC.java
@@ -0,0 +1,157 @@
+package com.zzrb.sign;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.*;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
+
+public class JDK8SignECC {
+
+
+    public static final String EC = "EC";    //密码原理
+    public static final String SIGNALGORITHM = "SHA256withECDSA";    //签名算法
+    public static final int KEYSIZE = 256; //大小
+
+    //私钥文件
+    public static final String fileName = "/private_key.json";
+    public static final String privKey = "priv_key";
+
+    //私钥文件中私钥
+    static String privateKeyStr;
+
+    static {
+        privateKeyStr = getPrivateKeyStrByResources();
+    }
+
+    //从classpath中获取private_key.json文件
+    public static String getPrivateKeyStrByResources(){
+        InputStream is = JDK8SignECC.class.getClass().getResourceAsStream(fileName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String s="";
+        String configContentStr = "";
+        try {
+            while((s=br.readLine())!=null) {
+                configContentStr = configContentStr+s;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject = JSONObject.parseObject(configContentStr);
+        return jsonObject.get(privKey).toString();
+    }
+
+//    //私钥默认读取路径
+//    public static final String userHome = "user.home";
+//    public static final String filePath = "/temp/key";
+//    // 初始化privateKeyStr，从用户目录的.anbo/key/private_key.json文件中读取
+//    public static String getPrivateKeyStr(){
+//        String home = System.getProperties().getProperty(userHome);
+//        String fullpath = home + filePath +"/" +fileName;
+//        String content = "";
+//        try {
+//            FileReader fileReader = new FileReader(fullpath);
+//            BufferedReader bufferedReader = new BufferedReader(fileReader);
+//            String strLine;
+//            while ((strLine = bufferedReader.readLine()) != null) {
+//                content =content + strLine;
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        JSONObject jsonObject = JSONObject.parseObject(content);
+//        return jsonObject.get(privKey).toString();
+//    }
+
+
+    // 公私钥生成
+    public  KeyPair initKey() throws Exception {
+        return initKey(KEYSIZE, new SecureRandom().generateSeed(8));
+    }
+
+    public KeyPair initKey(int keySize, byte[] seed) throws Exception {
+        KeyPairGenerator keygen = KeyPairGenerator.getInstance(JDK8SignECC.EC);
+        // 初始化随机产生器
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.setSeed(seed);
+        keygen.initialize(keySize, secureRandom);
+        KeyPair keys = keygen.genKeyPair();
+        return keys;
+    }
+
+    //获取公钥(Base64编码)
+    public String getPublicKey(KeyPair keyPair){
+        PublicKey publicKey = keyPair.getPublic();
+        byte[] bytes = publicKey.getEncoded();
+        return new String(Base64.getEncoder().encode(bytes));
+    }
+
+    //获取私钥(Base64编码)
+    public String getPrivateKey(KeyPair keyPair){
+        PrivateKey privateKey = keyPair.getPrivate();
+        byte[] bytes = privateKey.getEncoded();
+        return new String(Base64.getEncoder().encode(bytes));
+    }
+
+    //将Base64编码后的公钥转换成PublicKey对象
+    public PublicKey string2PublicKey(String pubStr) throws Exception{
+        byte[] keyBytes = Base64.getDecoder().decode(pubStr);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(JDK8SignECC.EC);
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+    //将Base64编码后的私钥转换成PrivateKey对象
+    public PrivateKey string2PrivateKey(String priStr) throws Exception{
+        byte[] keyBytes = Base64.getDecoder().decode(priStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(JDK8SignECC.EC);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    // 源数据的map,排序，拼接字符串：k1=v1k2=v2k3=v3,返回byte数组
+    public byte[] dataMap2byte(HashMap<String,String> data){
+        // 先将参数以其参数名的字典序升序进行排序
+        Map<String, String> sortedParams = new TreeMap<String, String>(data);
+        Set<Map.Entry<String, String>> entrys = sortedParams.entrySet();
+
+        // 遍历排序后的字典，将所有参数按"key=value"格式拼接在一起
+        StringBuilder basestring = new StringBuilder();
+        for (Map.Entry<String, String> param : entrys) {
+            basestring.append(param.getKey()).append("=").append(param.getValue());
+        }
+        return basestring.toString().getBytes();
+    }
+
+    //执行签名
+    public String sign(HashMap<String,String> data) throws Exception {
+        PrivateKey privateKey = string2PrivateKey(privateKeyStr);
+        byte[] dataBytes = dataMap2byte(data);
+        // 2.执行签名[私钥签名]
+        Signature signature = Signature.getInstance(JDK8SignECC.SIGNALGORITHM);
+        signature.initSign(privateKey);
+        signature.update(dataBytes);
+        return Base64.getEncoder().encodeToString(signature.sign());
+    }
+
+    // 验证签名
+    public boolean verify(String publKeyStr, HashMap<String,String> data, String sign) throws Exception {
+        PublicKey publicKey = string2PublicKey(publKeyStr);
+        byte[] signBytes = Base64.getDecoder().decode(sign);
+        byte[] dataBytes = dataMap2byte(data);
+        return verify(publicKey,dataBytes,signBytes);
+    }
+    // 验证签名
+    public boolean verify(PublicKey publicKey, byte[] data, byte[] sign) throws Exception {
+        // 3.验证签名[公钥验签]
+        Signature signature = Signature.getInstance(JDK8SignECC.SIGNALGORITHM);
+        signature.initVerify(publicKey);
+        signature.update(data);
+        return signature.verify(sign);
+    }
+
+}
---
+++ src/test/java/SignECCTest.java
@@ -0,0 +1,32 @@
+import com.alibaba.fastjson.JSONObject;
+import com.zzrb.sign.JDK8SignECC;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+public class SignECCTest {
+
+    @Test
+    public void sign() throws Exception {
+
+        JDK8SignECC jdk8SignECC = new JDK8SignECC();
+        HashMap<String,String> map = new HashMap<>();
+
+        map.put("accountType","1");
+        map.put("address","北京市朝阳区中电发展大厦");
+        map.put("name","张三");
+        map.put("bankName","招商银行");
+        map.put("accountNo","6225888888888888");
+        map.put("accountName","张三");
+
+        System.out.println("datamap:"+map);
+        String sign = jdk8SignECC.sign(map);
+        System.out.println("sign:"+sign);
+
+        String pubKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE1SmTD9+hRzZB4NXZnfPpRkRayW8RUQ0JeLAJEqap07C8MLe/jC4nP4b7SsNBfEzneZAdmn6gfXY7DwAXInBR6w==";
+        Boolean check = jdk8SignECC.verify(pubKey,map,sign);
+        System.out.println("check:"+check);
+
+    }
+
+}
